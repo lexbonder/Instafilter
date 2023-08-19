0ac90fde9d88b0041b823316598fdaa998c4b2c4
Alex Bonder <lexbonder@gmail.com>
2023-08-19 08:30:23 -0400

    Initial Commit

---
+++ Instafilter/InstafilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstafilterApp.swift
+//  Instafilter
+//
+//  Created by Alex Bonder on 8/19/23.
+//
+
+import SwiftUI
+
+@main
+struct InstafilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
