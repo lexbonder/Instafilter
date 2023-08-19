d8624751b48103760e013685c23478c3697a644b
Alex Bonder <lexbonder@gmail.com>
2023-08-19 08:47:45 -0400

    Hacking with Swift - 100 Days of SwiftUI - using onChange()

--- Instafilter/ContentView.swift
+++ Instafilter/ContentView.swift
@@ -8,14 +8,22 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var blurAmount = 0.0
+
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
             Text("Hello, world!")
+                .blur(radius: blurAmount)
+            
+            Slider(value: $blurAmount, in: 0...20)
+            
+            Button("Random Blur") {
+                blurAmount = Double.random(in: 0...20)
+            }
         }
-        .padding()
+        .onChange(of: blurAmount) { newValue in
+            print("New value is \(newValue)")
+        }
     }
 }
 
