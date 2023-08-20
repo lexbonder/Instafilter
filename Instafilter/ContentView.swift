ddd95533148e91f321579236f9fbc301317ad439
Alex Bonder <lexbonder@gmail.com>
2023-08-20 09:45:35 -0400

    Hacking with Swift - 100 Days of SwiftUI - Day 62

--- Instafilter/ContentView.swift
+++ Instafilter/ContentView.swift
@@ -8,14 +8,25 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var showingConfirmation = false
+    @State private var backgroundColor = Color.white
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
             Text("Hello, world!")
+                .frame(width: 300, height: 300)
+                .background(backgroundColor)
+                .onTapGesture {
+                    showingConfirmation = true
+                }
+                .confirmationDialog("Change Background", isPresented: $showingConfirmation) {
+                    Button("Red") { backgroundColor = .red }
+                    Button("Green") { backgroundColor = .green }
+                    Button("Blue") { backgroundColor = .blue }
+//                    Button("Cancel", role: .cancel) {} // Cancel button added by default
+                } message: {
+                    Text("Select a new color")
+                }
         }
-        .padding()
     }
 }
 
