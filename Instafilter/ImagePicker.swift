2cb319a06dcdeda57797ac959490b44041a50dd2
Alex Bonder <lexbonder@gmail.com>
2023-08-21 17:29:24 -0400

    Hacking with Swift - 100 Days of SwiftUI - Day 64 - Image Picker and Saver

--- Instafilter/ImagePicker.swift
+++ Instafilter/ImagePicker.swift
@@ -9,12 +9,45 @@ import PhotosUI
 import SwiftUI
 
 struct ImagePicker: UIViewControllerRepresentable {
+    @Binding var image: UIImage?
+    
+    class Coordinator: NSObject, PHPickerViewControllerDelegate {
+        var parent: ImagePicker
+        
+        init(_ parent: ImagePicker) {
+            self.parent = parent
+        }
+
+        func picker(_ picker: PHPickerViewController, didFinishPicking results: [PHPickerResult]) {
+            // Dismiss yourself
+            picker.dismiss(animated: true)
+            
+            // If results empty, user hit cancel
+            guard let provider = results.first?.itemProvider else { return }
+            
+            // If image, load it up!
+            if provider.canLoadObject(ofClass: UIImage.self) {
+                provider.loadObject(ofClass: UIImage.self) { image, _ in
+                    self.parent.image = image as? UIImage
+                }
+            }
+            
+        }
+    }
+    
     func makeUIViewController(context: Context) -> PHPickerViewController {
         var config = PHPickerConfiguration()
         config.filter = .images
         
-        return PHPickerViewController(configuration: config)
+        let picker = PHPickerViewController(configuration: config)
+        picker.delegate = context.coordinator
+        
+        return picker
     }
     
     func updateUIViewController(_ uiViewController: PHPickerViewController, context: Context) { }
+    
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self)
+    }
 }
