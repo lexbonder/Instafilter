ba8e31baa443309b1e02fb003b502e5c8231fb39
Alex Bonder <lexbonder@gmail.com>
2023-09-09 09:02:59 -0400

    Hacking with Swift - 100 Days of SwiftUI - Day 63
    Add comments

--- Instafilter/ContentView.swift
+++ Instafilter/ContentView.swift
@@ -22,12 +22,18 @@ struct ContentView: View {
     }
     
     func loadImage() {
+        // Load Picture as UIImage
         guard let inputImage = UIImage(named: "Example") else { return }
+        // Convert UIImage to CIImage
         let beginImage = CIImage(image: inputImage)
         
+        // CIContext converts CIImage data to CGImage
         let context = CIContext()
+        // CIFilter manipulates the CIImage data.
         let currentFilter = CIFilter.crystallize() // Has 2 properties, input image, and intensity - set both.
-        currentFilter.inputImage = beginImage
+        
+        // Current filter is a recipe. Give it the properties it needs to create the image wanted.
+        currentFilter.inputImage = beginImage // set the image for the filter to operate on.
         
         let amount = 1.0
         let inputKeys = currentFilter.inputKeys
@@ -44,10 +50,14 @@ struct ContentView: View {
             currentFilter.setValue(amount * 10, forKey: kCIInputScaleKey)
         }
         
+        // This property is the filter's processed image
         guard let outputImage = currentFilter.outputImage else { return }
         
+        // A method from context that spits out a CGImage based on the result of the filter
         if let cgimg = context.createCGImage(outputImage, from: outputImage.extent) {
+            // Convert to UIImage
             let uiImage = UIImage(cgImage: cgimg)
+            // Convert to SwiftUI Image and save to variable
             image = Image(uiImage: uiImage)
         }
     }
