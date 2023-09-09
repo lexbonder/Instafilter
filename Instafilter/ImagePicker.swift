020531e195720a4f17e71fe8ab060b2a05d96a82
Alex Bonder <lexbonder@gmail.com>
2023-09-09 09:17:13 -0400

    Hacking with Swift - 100 Days of SwiftUI - Day 63
    Add comments

--- Instafilter/ImagePicker.swift
+++ Instafilter/ImagePicker.swift
@@ -10,11 +10,17 @@ import SwiftUI
 
 struct ImagePicker: UIViewControllerRepresentable {
     func makeUIViewController(context: Context) -> PHPickerViewController {
+        // we can make our own adjustments to the picker
         var config = PHPickerConfiguration()
+        // we want to pick only images, ignore videos
         config.filter = .images
         
         return PHPickerViewController(configuration: config)
     }
     
+    // don't need to use this for this project
     func updateUIViewController(_ uiViewController: PHPickerViewController, context: Context) { }
+    
+    // used to generate correct protocol stubs
+//    typealias UIViewControllerType = PHPickerViewController
 }
